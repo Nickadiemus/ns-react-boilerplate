eeea030640b2e8f0956fa3dcc72bffdb6ebefc7e
Nick Sladic <nick.sladic@g.uky.edu>
2018-07-08 22:25:25 -0400

    boilerplate setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  //entry to bundled js file
+  entry: [
+    './src/index.js'
+  ],
+  //want to use the src/index.js file as entry point to bundle all of its imported files
+  module:{
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  //The bundled files will result in a bundle.js file which
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  //will be generated in our already set up /dist folder. The /dist folder will be used to serve our app.
+  devServer: {
+    contentBase: './dist'
+  }
+}
