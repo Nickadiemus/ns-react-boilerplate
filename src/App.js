d5ad9e0fab8f10df827b71d448c757ae2d31a1cd
Nick Sladic <nick.sladic@g.uky.edu>
2018-07-08 23:08:48 -0400

    merged dev with master

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/** main import(s)                                                      */
+import React, {Component} from 'react';
+
+/** helper import(s)                                                    */
+
+/** component import(s)                                                 */
+
+/** style import(s)                                                     */
+
+
+/*/
+ *  Component: App
+ *  @props {NA}
+ *  @EventHandler(s): NA
+ *  @Description: App page
+/*/
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <h2>This is the App Page</h2>
+      </div>
+    )
+  }
+}
+
+export default App
